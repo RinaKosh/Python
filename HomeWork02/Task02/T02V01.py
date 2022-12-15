79d294abe988fe346feccf27666d245901f311bd
Lichtermeer <amelterina@gmail.com>
2022-12-15 19:18:09 +0300

    Task02 Done

---
+++ HomeWork02/Task02/T02V01.py
@@ -0,0 +1,12 @@
+# Задайте список из n чисел последовательности (1 + 1/n)^n. Вывести в консоль сам список и сумму его элементов.
+
+n = int(input('Введите число '))
+my_list = []
+for i in range (n):
+    my_list.append((1 + 1/n)**n)
+print (*my_list, sep=', ')
+c = 0
+for i in my_list:
+    if i != '.':
+        c = float(i)+c
+print ('Сумма элементов равна', c)
