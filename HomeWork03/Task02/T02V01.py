a593e8d1e397e983d12379d9f22e0ab2136f032a
Lichtermeer <amelterina@gmail.com>
2022-12-19 17:04:23 +0300

    TASK02 DONE!

---
+++ HomeWork03/Task02/T02V01.py
@@ -0,0 +1,20 @@
+from random import randint as rnd
+
+my_list= []
+for i in range (4):
+    my_list.append(rnd(1,100))
+print(*my_list, sep=' ')
+
+last_index=len(my_list)-1
+c=[]
+for i in range(len(my_list)):
+    if i > last_index:
+        break
+    p=my_list[i]*my_list[last_index]
+    c.append(p)
+    last_index=last_index-1
+    print(p)
+print(f'Произведение пар чисел списка{my_list} равно {c}')
+
+    
+
